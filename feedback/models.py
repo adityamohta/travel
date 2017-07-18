d8ea94014fb9a75356a4d056f056583cc8dae5d1
aditya <adityamohta@bitbucket.org>
2017-07-18 18:29:43 +0530

    Added 2 more TODOs

--- feedback/models.py
+++ feedback/models.py
@@ -3,4 +3,5 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+# TODO 4: Add a testimonial model here
--- newsletter/models.py
+++ newsletter/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class Subscriptions(models.Model):
     email = models.EmailField()
-    # maybe add fields like subscribers' interests.
+    # TODO 3: maybe add fields like subscribers' interests.
 
     def __str__(self):
         return self.email
