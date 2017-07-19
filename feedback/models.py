8bc680c89e595629d2b36f1a6a3e49c5c1c85b3d
aditya <adityamohta@bitbucket.org>
2017-07-19 12:30:34 +0530

    feedback/models.py: Updated Testimonial Model

--- feedback/models.py
+++ feedback/models.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
-from accounts.models import Partner
-from django.core.validators import MinValueValidator,MaxValueValidator
 
-# TODO 4: Add a testimonial model here
+from accounts.models import Partner
 
 
 class Testimonial(models.Model):
     partner = models.ForeignKey(Partner, on_delete=models.CASCADE)
-    customer = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, max_length=255)
     review = models.TextField(blank=True)
-    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)], blank=True, null=True)
+    rating = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(5)],
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
-        return self.review
+        return self.user.username
